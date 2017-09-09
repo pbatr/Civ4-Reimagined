11682f815142aae0da9a8be744fad1a5780bc363
PierreB <pierre.batram@tupito.de>
2017-09-09 20:16:41 +0200

    "RiverSideYield" for improvements is now given for touching a river.

--- Civ4 Reimagined/Source/CvPlot.cpp
+++ Civ4 Reimagined/Source/CvPlot.cpp
@@ -6378,7 +6378,8 @@ int CvPlot::calculateImprovementYieldChange(ImprovementTypes eImprovement, Yield
 
 	iYield = GC.getImprovementInfo(eImprovement).getYieldChange(eYield);
 
-	if (isRiverSide())
+	//if (isRiverSide())
+	if (isRiver()) // K-Mod
 	{
 		iYield += GC.getImprovementInfo(eImprovement).getRiverSideYieldChange(eYield);
 	}
