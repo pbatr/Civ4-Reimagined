f91693fa76769ed6637b7b1c5d37907059b7344a
PierreB <pierre.batram@tupito.de>
2017-09-09 20:05:04 +0200

    The AI now ignore automation when taking over from human players.

--- Civ4 Reimagined/Source/CvSelectionGroup.cpp
+++ Civ4 Reimagined/Source/CvSelectionGroup.cpp
@@ -4234,6 +4234,7 @@ bool CvSelectionGroup::isAutomated() const
 void CvSelectionGroup::setAutomateType(AutomateTypes eNewValue)
 {
 	FAssert(getOwnerINLINE() != NO_PLAYER);
+	FAssert(isHuman() || eNewValue == NO_AUTOMATE); // The AI shouldn't use automation.
 
 	if (getAutomateType() != eNewValue)
 	{
