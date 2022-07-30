fcae5f630b2cb7045170e50490c301d9e763e868
pbatr <pierre.batram@tupito.de>
2022-07-31 00:18:56 +0200

    Fix great people rate bug during Autocracy

--- Civ4 Reimagined/Source/CvCity.cpp
+++ Civ4 Reimagined/Source/CvCity.cpp
@@ -12771,7 +12771,7 @@ void CvCity::setGreatPeopleUnitRate(UnitTypes eIndex, int iNewValue)
 
 void CvCity::changeGreatPeopleUnitRate(UnitTypes eIndex, int iChange)									 
 {
-	setGreatPeopleUnitRate(eIndex, (getGreatPeopleUnitRate(eIndex) + iChange));
+	setGreatPeopleUnitRate(eIndex, (m_paiGreatPeopleUnitRate[eIndex] + iChange));
 }
 
 
