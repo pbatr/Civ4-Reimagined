25b2babe07347d74da467b6267c34e553b42301d
pbatr <pierre.batram@tupito.de>
2022-08-08 22:16:50 +0200

    Viking UP: Melee units start with Amphibious

--- Civ4 Reimagined/Source/CvPlayer.cpp
+++ Civ4 Reimagined/Source/CvPlayer.cpp
@@ -30183,6 +30183,7 @@ void CvPlayer::updateUniquePowers(EraTypes eEra)
 		{
 			setCanCoastalRaid(true);
 			changeTerrainYieldChange((TerrainTypes)GC.getInfoTypeForString("TERRAIN_TUNDRA"), YIELD_COMMERCE, 1);
+			setFreePromotion((UnitCombatTypes)GC.getInfoTypeForString("UNITCOMBAT_MELEE"), (PromotionTypes)GC.getInfoTypeForString("PROMOTION_AMPHIBIOUS"), true);
 			notifyUniquePowersChanged(true);
 		}
 	}
