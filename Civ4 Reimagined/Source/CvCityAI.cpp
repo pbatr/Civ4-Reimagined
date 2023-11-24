57404ca7462e4368f701ef0c3b1b8ef4b853b997
pbatr <pierre.batram@tupito.de>
2023-11-24 11:16:12 +0100

    Adjust Floating Gardens food

--- Civ4 Reimagined/Source/CvCityAI.cpp
+++ Civ4 Reimagined/Source/CvCityAI.cpp
@@ -5153,7 +5153,7 @@ int CvCityAI::AI_buildingValue(BuildingTypes eBuilding, int iFocusFlags, int iTh
 					iTempValue += ((kBuilding.getSpecialistYieldChange(iJ, iI) * kOwner.getTotalPopulation()) / 5);
 				}
 				iTempValue += (kBuilding.getGlobalSeaPlotYieldChange(iI) * kOwner.countNumCoastalCities() * 8);
-				iTempValue += (kBuilding.getGlobalLakePlotYieldChange(iI) * std::max(4, iNumCities/4)); // Civ4 Reimagined
+				iTempValue += (kBuilding.getGlobalLakePlotYieldChange(iI) * std::max(4, iNumCities/2)); // Civ4 Reimagined
 				iTempValue += ((kBuilding.getAreaYieldModifier(iI) * iNumCitiesInArea) / 3);
 				iTempValue += ((kBuilding.getGlobalYieldModifier(iI) * iNumCities) / 3);
 
