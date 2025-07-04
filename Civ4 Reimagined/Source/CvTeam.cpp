74e8c1a0126fc3f3d4d934eec01a336562886e8d
Pierre Batram <pierre.batram@luca-app.de>
2025-07-04 17:38:14 +0200

    Political instability from Tech Progress

--- Civ4 Reimagined/Source/CvTeam.cpp
+++ Civ4 Reimagined/Source/CvTeam.cpp
@@ -5730,6 +5730,40 @@ void CvTeam::setHasTech(TechTypes eIndex, bool bNewValue, PlayerTypes ePlayer, b
 
 			// report event to Python, along with some other key state
 			CvEventReporter::getInstance().techAcquired(eIndex, getID(), ePlayer, bAnnounce);
+			
+			// Civ4 Reimagined: Add political instability when gaining new technology (excluding starting technologies)
+			// TODO: only for authoritarian ideologies
+			if (bNewValue && !GC.getTechInfo(eIndex).isRepeat() && GC.getTechInfo(eIndex).getEra() >= GC.getGameINLINE().getStartEra())
+			{
+				// Check if this is a civilization-specific starting technology
+				bool bIsCivStartingTech = false;
+				for (int iI = 0; iI < MAX_PLAYERS; iI++)
+				{
+					if (GET_PLAYER((PlayerTypes)iI).getTeam() == getID())
+					{
+						if (GC.getCivilizationInfo(GET_PLAYER((PlayerTypes)iI).getCivilizationType()).isCivilizationFreeTechs(eIndex))
+						{
+							bIsCivStartingTech = true;
+							break;
+						}
+					}
+				}
+			}
+			
+			// Only add instability if it's not a civilization starting technology
+			if (!bIsCivStartingTech)
+			{
+				// Era-based instability: more advanced technologies cause more disruption
+				int iInstability = 3 * (GC.getTechInfo(eIndex).getEra() + 1); // Era 0 = 3, Era 1 = 6, etc.
+				
+				for (iI = 0; iI < MAX_PLAYERS; iI++)
+				{
+					if (GET_PLAYER((PlayerTypes)iI).getTeam() == getID())
+					{
+						GET_PLAYER((PlayerTypes)iI).changePoliticalInstabilityProgress(iInstability, "Technology Advance");
+					}
+				}
+			}
 
 			bReligionFounded = false;
 			bFirstBonus = false;
